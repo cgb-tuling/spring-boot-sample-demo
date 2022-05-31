71909131c6e6ed481186317ef48d28fec253cbd4
tian <1447660461@qq.com>
2022-05-31 14:57:42 +0800

    SpringbootTransationAfterCommitApplication

--- springboot-learning-master/springboot-transation-after-commit/src/main/java/com/github/lybgeek/SpringbootTransationAfterCommitApplication.java
+++ springboot-learning-master/springboot-transation-after-commit/src/main/java/com/github/lybgeek/SpringbootTransationAfterCommitApplication.java
@@ -3,8 +3,10 @@ package com.github.lybgeek;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EnableTransactionManagement
 @MapperScan(basePackages = "com.github.lybgeek.user.**.dao")
 public class SpringbootTransationAfterCommitApplication {
 
